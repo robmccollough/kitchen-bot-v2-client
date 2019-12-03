093a249c0dd0975a3565250d70276c7bdb976d2f
Tyler Ording <tylerkording@gmail.com>
2019-12-03 18:06:35 -0500

    new

--- src/components/new/home/index.js
+++ src/components/new/home/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom';
-import { Provider, connect, withRouter } from 'react-redux';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
+import { Provider, connect, withRouter } from "react-redux";
 
-const Home = (props) => {
-
-    //put the cookie check/redirect here
-    return <div>
-        <h1>Home</h1>
-        <Link to='/home'>Home</Link>
-        <Link to='/home/admin'>Admin</Link>
-        <Link to='/register'>Register</Link>
+const Home = props => {
+  //put the cookie check/redirect here
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="./login">Login</Link>
+      <Link to="/home">Home</Link>
+      <Link to="/home/admin">Admin</Link>
+      <Link to="/register">Register</Link>
     </div>
+  );
+};
 
-}
-
-export default Home
+export default Home;
--- src/components/new/register/index.js
+++ src/components/new/register/index.js
@@ -1,56 +1,80 @@
-import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom';
-import { Provider, connect, withRouter } from 'react-redux';
-import useForm from 'react-hook-form'
-import axios from 'axios'
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
+import { Provider, connect, withRouter } from "react-redux";
+import useForm from "react-hook-form";
+import axios from "axios";
 
+const Register = props => {
+  const { handleSubmit, register, errors, getValues } = useForm();
+  console.log(errors);
+  const onSubmit = values => {
+    console.log(values);
+  };
 
-const Register = (props) => {
+  //returns true if email is use
+  const checkEmail = async email => {
+    return await axios({
+      method: "post",
+      url: "/user/check",
+      body: {
+        email: email
+      }
+    }).then(result => {
+      return result.in_use;
+    });
+  };
 
-    const {handleSubmit, register, errors, getValues} = useForm();
-    console.log(errors)
-    const onSubmit = values => {
-        console.log(values)
-    }
+  //put the cookie check/redirect here
 
-    //returns true if email is use
-    const checkEmail = async (email) => {
-        return await axios({
-            method: 'post',
-            url: '/user/check',
-            body:{
-                email: email
-            }
-        }).then( result => {
-            return result.in_use
-        })
-    }
-
-
-    //put the cookie check/redirect here
-
-
-    
-    return <div>
-        <h1>Register</h1>
-        <Link to='/home'>Clink</Link>
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="Email" name="Email" ref={register({
-            required: true, 
+  return (
+    <div>
+      <h1>Register</h1>
+      <Link to="/home">Click</Link>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Email"
+          name="Email"
+          ref={register({
+            required: true,
             pattern: /^\S+@\S+$/i,
             validate: value => checkEmail(value)
-            })} />
-            {errors.email && "Email already in use"}
-        <input type="text" placeholder="Password" name="Password" ref={register({required: true})} />
-        <input type="text" placeholder="Confirm Password" name="Password" ref={register({required: true, validate: value => {
-            console.log(getValues())
-        }})} />
-        <input type="checkbox" placeholder="Link GroupMe Account?" name="Link GroupMe Account?" ref={register} />
+          })}
+        />
+        {errors.email && "Email already in use"}
+        <input
+          type="text"
+          placeholder="Password"
+          name="Password"
+          ref={register({ required: true })}
+        />
+        <input
+          type="text"
+          placeholder="Confirm Password"
+          name="Password"
+          ref={register({
+            required: true,
+            validate: value => {
+              console.log(getValues());
+            }
+          })}
+        />
+        <input
+          type="checkbox"
+          placeholder="Link GroupMe Account?"
+          name="Link GroupMe Account?"
+          ref={register}
+        />
 
         <input type="submit" />
-        </form>
+      </form>
     </div>
+  );
+};
 
-}
-
-export default Register
+export default Register;
